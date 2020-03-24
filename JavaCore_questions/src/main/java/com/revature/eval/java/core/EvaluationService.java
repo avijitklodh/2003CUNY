a6fac53d12e3ec9cd69856650a72011fa57ef521
Will Newell <newellwm@gmail.com>
2020-03-24 19:04:34 -0400

    Add Triangle classification methods

--- JavaCore_questions/src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ JavaCore_questions/src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -90,18 +90,17 @@ public class EvaluationService {
 		}
 
 		public boolean isEquilateral() {
-			// TODO Write an implementation for this method declaration
-			return false;
+			return this.sideOne == this.sideTwo && this.sideTwo == this.sideThree;
 		}
 
 		public boolean isIsosceles() {
-			// TODO Write an implementation for this method declaration
-			return false;
+			return this.sideOne == this.sideTwo
+					|| this.sideOne == this.sideThree
+					|| this.sideTwo == this.sideThree;
 		}
 
 		public boolean isScalene() {
-			// TODO Write an implementation for this method declaration
-			return false;
+			return !isIsosceles();
 		}
 
 	}
