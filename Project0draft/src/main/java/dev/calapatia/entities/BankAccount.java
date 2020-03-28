45d51367fac3c7c230281685f2d0e6c962124f8c
Raymond <raymond.50cal93@gmail.com>
2020-03-27 21:47:53 -0400

    update
    update to code

--- Project0draft/src/main/java/dev/calapatia/entities/BankAccount.java
+++ Project0draft/src/main/java/dev/calapatia/entities/BankAccount.java
@@ -1,5 +1,41 @@
 package dev.calapatia.entities;
 
 public class BankAccount {
+	
+	// Primary key
+	private int bankAccountId;
+	private int balance;
+	
+	//foreign key
+	private int userId;
+
+	public int getBankAccountId() {
+		return bankAccountId;
+	}
+
+	public void setBankAccountId(int bankAccountId) {
+		this.bankAccountId = bankAccountId;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "BankAccount [bankAccountId=" + bankAccountId + ", balance=" + balance + ", userId=" + userId + "]";
+	}
 
 }
--- Project0draft/src/main/java/dev/calapatia/entities/User.java
+++ Project0draft/src/main/java/dev/calapatia/entities/User.java
@@ -1,5 +1,56 @@
 package dev.calapatia.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
+	
+	// Primary key
+	private int userId;
+	
+	private String username;
+	
+	private String password;
+	
+	Set<BankAccount> accounts = new HashSet<BankAccount>();
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<BankAccount> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(Set<BankAccount> accounts) {
+		this.accounts = accounts;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", accounts=" + accounts
+				+ "]";
+	}
+	
 
 }
