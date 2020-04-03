eceae64e85ac1741cab74dc7a11d811a4afaee9d
Will Newell <newellwm@gmail.com>
2020-04-03 11:46:05 -0400

    Initialize AccoundDao db implementation with stubs

---
+++ BankOfWill/src/main/java/codes/newell/dao/AccountDAOdb.java
@@ -0,0 +1,45 @@
+package codes.newell.dao;
+
+import java.util.List;
+
+import codes.newell.entities.Account;
+
+public class AccountDAOdb implements AccountDAO {
+
+	@Override
+	public Account getAccountById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Account> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Account> getAllAccountsByUserId(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account createAccount(Account account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account updateAccount(Account account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
