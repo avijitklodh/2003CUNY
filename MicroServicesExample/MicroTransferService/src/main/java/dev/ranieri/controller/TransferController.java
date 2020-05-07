ff6d13697b13c05d9283aafa0c014a1bae35bc19
Adam_Ranieri <adam.ranieri@revature.com>
2020-05-07 09:07:46 -0400

    added rest of microservices

--- MicroServicesExample/MicroTransferService/src/main/java/dev/ranieri/controller/TransferController.java
+++ MicroServicesExample/MicroTransferService/src/main/java/dev/ranieri/controller/TransferController.java
@@ -1,5 +1,7 @@
 package dev.ranieri.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,19 +9,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import dev.ranieri.services.PointTransferService;
 
 @Controller
 public class TransferController {
+	
+	private static final Logger logger = LogManager.getLogger();
 	
 	@Autowired
 	PointTransferService pts;
 
 	@RequestMapping(value = "/transfer/sender/{senderid}/recipient/{recipientid}/amount/{amount}")
 	@ResponseBody
+	@HystrixCommand(fallbackMethod = "localLog")
 	public String transferPoints(@PathVariable String senderid, @PathVariable String recipientid, @PathVariable String amount ) {
 		
 		pts.transferPoints(Integer.parseInt(senderid), Integer.parseInt(recipientid), Integer.parseInt(amount));
+		
+		if(true) {
+			throw new RuntimeException();
+		}
+		
 		return "Transfer was successful";
+	}
+	
+	public String localLog(String senderid,String recipientid, String amount) {
+		logger.info(senderid+ " tranfer to "+ recipientid +"  amount:"+amount);
+		System.out.println(senderid+ " tranfer to "+ recipientid +"  amount:"+amount);
+		return "Assoicate service is down. Transfer was saved locally. Will be updated when Associate service resumes";
 	}
 }
